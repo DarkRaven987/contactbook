6f919750477d2dbb7b7f2dd487c3c0285a350ce8
DarkRaven987 <darkraven987@gmail.com>
2019-01-22 18:57:12 +0200

    Created input form for new contact

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
 
@@ -12,6 +11,7 @@ class App extends Component {
     super(props);
 
     this.state = {
+        newContact: {id: 0, number: '', firstName: '', secondName: '', company:  '',email: '', avatar: ""},
 
     }
   }
@@ -20,33 +20,78 @@ class App extends Component {
     const {contacts} = this.props;
     return (
       <div className="App">
-        <div className="ui container">
-          <div className="ui menu">
-              <div className="ui item">Contact book</div>
-              <div className="ui item right">About us</div>
-          </div>
-          <div className="c ui five column grid segment">
-              {contacts.map((el) => {
-                return(
-                    <div className="column">
-                        <div className="ui card">
-                            <div className="image">
-                                <img src={el.avatar}/>
+            <div className="ui container">
+                <div className="ui icon labeled menu">
+                      <div className="item"><i className="icon phone square"></i>Contact book</div>
+                </div>
+
+                <div className="ui segment">
+                    <div className="inputMenu ui segment">
+                        <div className="ui form three column grid">
+                            <div className="field column">
+                                <label>Name</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="Name"/>
+                                </div>
                             </div>
-                            <div className="content">
-                                <a className="header">{`${el.firstName} ${el.secondName}`}</a>
-                                <div className="description">
-                                    <p>Number: {el.number}</p>
-                                    <p>Company: {el.company}</p>
-                                    <p>E-mail: {el.email}</p>
+                            <div className="field column">
+                                <label>Surname</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="Surname"/>
+                                </div>
+                            </div>
+                            <div className="field column">
+                                <label>Contact number</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="Number"/>
+                                </div>
+                            </div>
+                            <div className="field column">
+                                <label>E-mail</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="E-mail"/>
+                                </div>
+                            </div>
+                            <div className="field column">
+                                <label>Company</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="Company"/>
+                                </div>
+                            </div>
+                            <div className="field column">
+                                <label>Avatar</label>
+                                <div className="ui input">
+                                    <input type="text" placeholder="Image URL"/>
                                 </div>
                             </div>
                         </div>
+                        <button className="ui inverted button big blue">Create</button>
                     </div>
-                );
-              })}
+
+                        <div className="outputMenu">
+                            <div className="ui four cards">
+                                {contacts.map((el) => {
+                                    return(
+                                        <div className="ui card" key={el.id}>
+                                            <div className="image">
+                                                <img src={el.avatar} alt="/"/>
+                                            </div>
+                                            <div className="content">
+                                                <a className="header">{`${el.firstName} ${el.secondName}`}</a>
+                                                <div className="description">
+                                                    <p className='ui text'>Number: {el.number}</p>
+                                                    <p>Company: {el.company}</p>
+                                                    <p>E-mail: {el.email}</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+
+            </div>
           </div>
-        </div>
       </div>
     );
   }
