55ea6ebbfff4f7f1a791d1dca1b9c4e1b0058f0d
DarkRaven987 <darkraven987@gmail.com>
2019-01-24 00:10:54 +0200

    Got buttons divided in other class

---
+++ src/components/MainBody/InputMenu/buttons.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {connect} from "react-redux";
+import {bindActionCreators} from "redux";
+
+import {changeEditMode, createContact, editContact} from "../../../store/actions";
+import {checkDataInput, clearInputs} from "../functions";
+import {inputs, warnings} from "../collections";
+
+class Buttons extends React.Component {
+    render(){
+        let {editMode, contacts, editContact, changeEditMode, createContact, editedContact, newContact, setNewContact, setEditedContact} = this.props;
+        return(
+            <div>
+                {editMode?
+                    <div>
+                        <button className="ui inverted button big blue"
+                                onClick={async() => {
+                                    await changeEditMode(false);
+                                    await setEditedContact(editedContact);
+                                    await editContact(editedContact);
+                                    clearInputs(inputs);
+                                    console.log(editedContact);
+                                }}>Save</button>
+                        <button className="ui inverted secondary big button"
+                                onClick={async() => {
+                                    await changeEditMode(false);
+                                    clearInputs(inputs);
+                                }}>Cancel</button>
+                    </div>
+
+                    :
+                    <button className="ui inverted button big green"
+                            onClick={async () => {
+                                let result = checkDataInput(inputs,warnings);
+                                if(result===false){
+                                    newContact.id = contacts[contacts.length-1].id+1;
+                                    await setNewContact(newContact);
+                                    await createContact(newContact);
+                                    clearInputs(inputs);
+                                }
+                            }
+                            }>Create</button> }
+            </div>
+        );
+    }
+
+}
+
+const putStateToProps = (state) => {
+    return{
+        contacts: state.contacts,
+        editMode: state.editMode
+    }
+};
+
+const putActionsToProps = (dispatch) => {
+    return {
+        changeEditMode: bindActionCreators(changeEditMode, dispatch),
+        createContact: bindActionCreators(createContact, dispatch),
+        editContact: bindActionCreators(editContact, dispatch),
+    }
+};
+
+export default connect(putStateToProps, putActionsToProps)(Buttons);
---
+++ src/components/MainBody/InputMenu/index.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import {connect} from "react-redux";
+
+import Buttons from './buttons';
+
+class InputMenu extends React.Component {
+
+    render(){
+        const {getEditedContact, getNewContact, setEditedContact, setNewContact} = this.props;
+
+        let editedContact = getEditedContact();
+        let newContact = getNewContact();
+
+        const {editMode} = this.props;
+
+        return(
+            <div className="inputMenu ui segment">
+                <div className="ui form three column grid">
+                    <div className="field column">
+                        <label>Name</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="Name"
+                                   onChange={
+                                       editMode?
+                                           (e) => {editedContact.firstName = e.target.value; console.log(editedContact)}
+                                           : (e) => {newContact.firstName = e.target.value;}
+
+                                   }/>
+                        </div>
+                        <div id='name' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Please enter a value
+                        </div>
+                    </div>
+                    <div className="field column">
+                        <label>Surname</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="Surname"
+                                   onChange={editMode?
+                                       (e) => {editedContact.secondName = e.target.value}
+                                       : (e) => {newContact.secondName = e.target.value}
+                                   }/>
+                        </div>
+                        <div id='surname' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Please enter a value
+                        </div>
+                    </div>
+                    <div className="field column">
+                        <label>Contact number</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="Contact number"
+                                   onChange={editMode?
+                                       (e) => {editedContact.number = e.target.value}
+                                       : (e) => {newContact.number = e.target.value}
+                                   }/>
+                        </div>
+                        <div id='contact' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Please enter a value
+                        </div>
+                        <div id='contactFormat' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Wrong format! Should be like this: 0XX-XXX-XX-XX
+                        </div>
+                    </div>
+                    <div className="field column">
+                        <label>E-mail</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="E-mail"
+                                   onChange={editMode?
+                                       (e) => {editedContact.email = e.target.value}
+                                       : (e) => {newContact.email = e.target.value}
+                                   }/>
+                        </div>
+                        <div id='email' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Please enter a value
+                        </div>
+                        <div id='emailFormat' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Wrong format! Should be like this: example@email.org
+                        </div>
+                    </div>
+                    <div className="field column">
+                        <label>Company</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="Company"
+                                   onChange={editMode?
+                                       (e) => {editedContact.company = e.target.value}
+                                       : (e) => {newContact.company = e.target.value}
+                                   }/>
+                        </div>
+                    </div>
+                    <div className="field column">
+                        <label>Avatar</label>
+                        <div className="ui input">
+                            <input className="inputField" type="text" placeholder="URL-address"
+                                   onChange={editMode?
+                                       (e) => {editedContact.avatar = e.target.value}
+                                       : (e) => {newContact.avatar = e.target.value}
+                                   }/>
+                        </div>
+                        <div id='avatar' className="ui pointing label basic red warning" style={{display: 'none'}}>
+                            Please enter a value
+                        </div>
+                    </div>
+                </div>
+
+                <Buttons
+                    setEditedContact={setEditedContact}
+                    setNewContact={setNewContact}
+                    newContact={newContact}
+                    editedContact={editedContact}
+                />
+
+            </div>
+        );
+    }
+
+}
+
+const putStateToProps = (state) => {
+    return{
+        editMode: state.editMode
+    }
+};
+
+export default connect(putStateToProps)(InputMenu);
