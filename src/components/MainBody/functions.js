495a75a5c1d2d7d64f93c69400f4fe28cd2aae96
DarkRaven987 <darkraven987@gmail.com>
2019-01-24 00:09:22 +0200

    Get out renderCard function

--- src/components/MainBody/functions.js
+++ src/components/MainBody/functions.js
@@ -67,47 +67,4 @@ let clearInputs = (inputs) => {
     inputs[5].value = '';
 };
 
-let renderCardList = (arr, inputs) => {
-    return(
-        arr.slice(0).reverse().map((el) => {
-            return(
-                <div className="ui card" key={el.id}>
-                    <div className="image">
-                        <img src={el.avatar} alt="/"/>
-                    </div>
-                    <div className="content">
-                        <a className="header">{`${el.firstName} ${el.secondName}`}</a>
-                        <div className="description">
-                            <p className='ui text'>Number: {el.number}</p>
-                            <p className='ui text'>Company: {el.company}</p>
-                            <p className='ui text'>E-mail: {el.email}</p>
-                        </div>
-                    </div>
-                    <div className="ui buttons">
-                        <button className="ui button blue inverted"
-                                onClick={  () => {
-                                    this.props.changeEditMode(true);
-                                    this.setState({editedContact: el});
-                                    inputs[0].value = el.firstName;
-                                    inputs[1].value = el.secondName;
-                                    inputs[2].value = el.number;
-                                    inputs[3].value = el.email;
-                                    inputs[4].value = el.company;
-                                    inputs[5].value = el.avatar;
-                                }}
-                        ><i className="icon edit outline"/></button>
-                        <div className="or"/>
-                        <button className="ui button red inverted" onClick={() => {
-                            this.props.deleteContact(el.id);
-                            this.setState({editedContact: {id: 0, number: "", firstName: "", secondName: "", company:  "-",email: "", avatar: ""}})
-                        }}>
-                            <i className="icon trash alternate outline"/>
-                        </button>
-                    </div>
-                </div>
-            );
-        })
-    );
-};
-
-export {checkDataInput, clearInputs, renderCardList}
+export {checkDataInput, clearInputs}
